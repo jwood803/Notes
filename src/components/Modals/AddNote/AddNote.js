18b2e503822fdf1468f0123a162e0aa3364a3f58
J W <woodjon@gmail.com>
2018-03-27 09:02:07 -0400

    Build new note form

--- src/components/Modals/AddNote/AddNote.js
+++ src/components/Modals/AddNote/AddNote.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {Modal, Button} from "react-bootstrap";
+import {Modal, Button, Form, FormGroup, ControlLabel, FormControl, Radio, Checkbox} from "react-bootstrap";
+import _times from "lodash/times";
 
 const addNote = props => {
   let {
@@ -8,13 +9,52 @@ const addNote = props => {
     addNote
   } = props;
 
+  const RATING_COUNT = 5;
+
+  let FieldGroup = ({ id, label, help, ...props }) => {
+    return (
+      <FormGroup controlId={id}>
+        <ControlLabel>{label}</ControlLabel>
+        <FormControl {...props} />
+      </FormGroup>
+    );
+  };
+
+  let form = (
+    <Form>
+      <FieldGroup
+        id="title"
+        type="text"
+        label="Book Title"
+        placeholder="Enter text"
+      />
+      <FormGroup controlId="noteDetails">
+        <ControlLabel>Note Details</ControlLabel>
+        <FormControl componentClass="textarea" placeholder="textarea" />
+      </FormGroup>
+      <FormGroup>
+        {
+          _times(RATING_COUNT, cnt => {
+            return (
+            <Radio name="radioGroup" inline>
+              {cnt + 1}
+            </Radio>)
+          })
+        }
+      </FormGroup>
+      <Checkbox>
+        Favorite?
+      </Checkbox>
+    </Form>
+  );
+
   return (
     <Modal show={showModal} onHide={closeModal}>
       <Modal.Header>
         <Modal.Title>Add New Note</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        TODO: Add note form
+        {form}
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={closeModal}>Close</Button>
