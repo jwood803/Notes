f7778155107fee5e86e7e2454f9145f84411d816
J W <woodjon@gmail.com>
2018-03-30 11:10:07 -0400

    Get started adding authentication

---
+++ src/containers/Authentication/Authentication.js
@@ -0,0 +1,14 @@
+import React, {Component, Fragment} from "react";
+import {connect} from "react-redux";
+
+class Authentication extends Component {
+  render() {
+    return (
+      <Fragment>
+
+      </Fragment>
+    );
+  }
+}
+
+export default connect()(Authentication);
