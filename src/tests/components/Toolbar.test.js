7d47bcea1c5b335d8069e799e2e27dac0a899031
J W <woodjon@gmail.com>
2018-03-31 12:59:38 -0400

    Add more tests

--- src/tests/components/Toolbar.test.js
+++ src/tests/components/Toolbar.test.js
@@ -2,14 +2,27 @@ import React from "react";
 import {configure, shallow} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
-import {Nav} from "react-bootstrap";
+import {Nav, Navbar} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
 
 configure({adapter: new Adapter()});
 
-describe("Toolbar", () => {
-  it("should render 3 Nav components", () => {
-    const wrapper = shallow(<Toolbar/>);
+let wrapper = null;
 
+describe("Toolbar", () => {
+  beforeEach(() => {
+    wrapper = shallow(<Toolbar/>);
+  });
+
+  it("should render 3 Nav components", () => {
     expect(wrapper.find(Nav)).toHaveLength(3);
+  });
+
+  it("should render a navbar header", () => {
+    expect(wrapper.find(Navbar.Header)).toHaveLength(1);
+  });
+
+  it("should render 3 LinkContainer components", () => {
+    expect(wrapper.find(LinkContainer)).toHaveLength(3);
   });
 });
